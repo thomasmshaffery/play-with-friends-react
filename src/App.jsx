47527c53fa9287c8cefbdd32dd42bf4bd4b6c738
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-06-07 13:51:24 -0400

    Dummy party data and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,67 @@ import Party from "./pages/Party";
 import Homepage from "./pages/Homepage";
 import Games from "./pages/Games";
 import PageNotFound from "./pages/PageNotFound";
+import { useReducer } from "react";
+
+const initialState = {
+  partyMembers: [
+    // {
+    //   name: "tom",
+    //   age: 34,
+    // },
+    // {
+    //   name: "matt",
+    //   age: 35,
+    // },
+    // {
+    //   name: "Mooch",
+    //   age: "babby",
+    // },
+    // {
+    //   name: "Noodles",
+    //   age: "babby",
+    // },
+  ],
+  games: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "addUser":
+      return {
+        ...state,
+        partySize: state.partySize + 1,
+      };
+    default:
+      return { state };
+  }
+}
 
 function App() {
+  const [{ partyMembers, games }, dispatch] = useReducer(reducer, initialState);
+  const partySize = partyMembers.length;
+  const numOfGames = games.length;
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="party" element={<Party />} />
-        <Route path="games" element={<Games />} />
+        <Route
+          path="party"
+          element={
+            <Party
+              partyMembers={partyMembers}
+              partySize={partySize}
+              dispatch={dispatch}
+            />
+          }
+        />
+        <Route
+          path="games"
+          element={
+            <Games games={games} numOfGames={numOfGames} dispatch={dispatch} />
+          }
+        />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Party.jsx
+++ src/pages/Party.jsx
@@ -1,12 +1,18 @@
 import PageNav from "../components/PageNav";
 import styles from "./Party.module.css";
 
-function Party() {
+function Party({ partyMembers, partySize, dispatch }) {
   return (
     <main className={styles.party}>
       <PageNav />
       <section>
-        <h1>Party Creator</h1>
+        {partySize > 0 ? (
+          (<h1>Here are your party members:</h1>)(
+            partyMembers.map((member) => console.log(member.name))
+          )
+        ) : (
+          <h1>There is no one in your party!</h1>
+        )}
       </section>
     </main>
   );
