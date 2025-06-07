b2209c93041d38158256575fcdf0938491f9624c
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-06-07 00:54:52 -0400

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Party from "./pages/Party";
+import Homepage from "./pages/Homepage";
+import Games from "./pages/Games";
+
 function App() {
-  return <div>Play With Friends</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      <Routes>
+        <Route path="party" element={<Party />} />
+      </Routes>
+      <Routes>
+        <Route path="games" element={<Games />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
